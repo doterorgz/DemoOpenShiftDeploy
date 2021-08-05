5ae7d111bdd311b014cbc5aeffb51815d2b7e213
Daniel Otero Rodríguez <daniel.otero.rgz@gmail.com>
2021-08-05 21:38:04 +0200

    Init

---
+++ src/main/java/es/doterorgz/demoopenshiftdeploy/DemoOpenShiftDeployApplication.java
@@ -0,0 +1,13 @@
+package es.doterorgz.demoopenshiftdeploy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoOpenShiftDeployApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoOpenShiftDeployApplication.class, args);
+    }
+
+}
---
+++ src/main/java/es/doterorgz/demoopenshiftdeploy/HelloController.java
@@ -0,0 +1,17 @@
+package es.doterorgz.demoopenshiftdeploy;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/")
+public class HelloController {
+
+    @GetMapping("/hello")
+    public String sayHelloy(@PathVariable String name) {
+        return "Hello " + name + "!!!";
+    }
+
+}
